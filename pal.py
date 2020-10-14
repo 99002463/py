9577ac4e34888e5e0528c2f7f40f72a72ac7c6ad
99002463 <71491422+99002463@users.noreply.github.com>
2020-10-14 11:46:40 +0530

    Create pal.py

---
+++ pal.py
@@ -0,0 +1,8 @@
+def isPalindrome(inputUser):
+    palindrome_input = inputUser
+    rev = ''.join(reversed(palindrome_input))
+    if palindrome_input == rev:
+        return True
+    return False
+def add(x,y):
+    return x+y
